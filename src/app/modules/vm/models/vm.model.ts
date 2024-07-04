8dc6c30d343a158c1b7fd3dfdfe635e5c55aaf00
JonFdz <jonfernal@gmail.com>
2024-07-04 14:57:29 +0200

    Updated vm model

--- src/app/modules/vm/models/vm.model.ts
+++ src/app/modules/vm/models/vm.model.ts
@@ -1,5 +1,18 @@
 export interface VirtualMachine {
-	id: string;
-	name: string;
+	id?: number;
+	vmName: string;
+	ip: string;
+	dnsName: string;
+	project?: string;
+	environment?: string;
 	status: string;
+	comment?: string;
+	reservedFrom?: Date;
+	reservedTo?: Date;
+	operatingSystem?: string;
+	cpuCores?: number;
+	ram?: number;
+	disk?: number;
+	createdAt?: Date;
+	updatedAt?: Date;
 }
