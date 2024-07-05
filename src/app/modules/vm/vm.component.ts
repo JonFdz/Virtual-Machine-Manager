5200fe19870039b874c960b8618a7bc4c3f9beb1
JonFdz <jonfernal@gmail.com>
2024-07-05 14:20:21 +0200

    Added vm Details and Form to vm component

--- src/app/modules/vm/vm.component.ts
+++ src/app/modules/vm/vm.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VmDetailsComponent } from './components/vm-details/vm-details.component';
+import { VmListComponent } from './components/vm-list/vm-list.component';
+import { VmFormComponent } from './components/vm-form/vm-form.component';
 
 @Component({
   selector: 'app-vm',
   templateUrl: './vm.component.html',
   styleUrl: './vm.component.scss',
   standalone: true,
-  imports: [ CommonModule, VmDetailsComponent ]
+  imports: [ CommonModule, VmDetailsComponent, VmListComponent, VmFormComponent ]
 })
 export class VmComponent {
 
