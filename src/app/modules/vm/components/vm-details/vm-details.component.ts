591272c420987d85aafeef514c2b5ed8e1b0a5f8
JonFdz <jonfernal@gmail.com>
2024-07-05 12:08:48 +0200

    Added HTML to vm-details

--- src/app/modules/vm/components/vm-details/vm-details.component.ts
+++ src/app/modules/vm/components/vm-details/vm-details.component.ts
@@ -2,22 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { VmService } from '@vm/services/vm.service';
 import { VirtualMachine } from '@vm/models/vm.model';
+import { DateFormatterPipe } from '@shared/pipes/date-formatter.pipe';
 
 @Component({
 	selector: 'app-vm-details',
 	templateUrl: './vm-details.component.html',
-	styleUrls: ['./vm-details.component.scss']
+	styleUrls: ['./vm-details.component.scss'],
+	standalone: true,
+	imports: [ DateFormatterPipe ]
 })
 export class VmDetailsComponent implements OnInit {
 	vm: VirtualMachine = {} as VirtualMachine;
 
 	constructor(
 		private route: ActivatedRoute,
-		private vmService: VmService
+		private vmService: VmService,
 	) { }
 
 	ngOnInit(): void {
-		const id = this.route.snapshot.paramMap.get('id');
+		const id = 1;
 		if (id) {
 			this.vmService.getVm(id).subscribe(data => {
 				this.vm = data;
