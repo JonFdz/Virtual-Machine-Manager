4322ad56ae47960009c453cee2d3a1561f1f2dcf
JonFdz <jonfernal@gmail.com>
2024-07-07 13:43:18 +0200

    Added header component to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { VmComponent } from '@vm/vm.component';
+import { HeaderComponent } from './modules/header/header.component';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, VmComponent],
+	imports: [RouterOutlet, VmComponent, HeaderComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss'
 })
