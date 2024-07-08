ad4f41c70c70fe4948eda4a5f592b464c6ba0b42
JonFdz <jonfernal@gmail.com>
2024-07-08 11:36:36 +0200

    Updated vm-component and vm-routes with new compoanents

--- src/app/modules/vm/vm.component.ts
+++ src/app/modules/vm/vm.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VmDetailsComponent } from './components/vm-details/vm-details.component';
 import { VmListComponent } from './components/vm-list/vm-list.component';
-import { VmFormComponent } from './components/vm-form/vm-form.component';
+import { VmRegisterComponent } from './components/vm-register/vm-register.component';
+import { VmEditComponent } from './components/vm-edit/vm-edit.component';
+import { VmDialogComponent } from './components/vm-dialog/vm-dialog.component';
 
 @Component({
   selector: 'app-vm',
   templateUrl: './vm.component.html',
   styleUrl: './vm.component.scss',
   standalone: true,
-  imports: [ CommonModule, VmDetailsComponent, VmListComponent, VmFormComponent ]
+  imports: [ CommonModule, VmDetailsComponent, VmListComponent, VmRegisterComponent, VmEditComponent, VmDialogComponent ],
 })
 export class VmComponent {
 
--- src/app/modules/vm/vm.routes.ts
+++ src/app/modules/vm/vm.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
+
 import { VmListComponent } from '@vm/components/vm-list/vm-list.component';
-import { VmDetailsComponent } from '@vm/components/vm-details/vm-details.component';
-import { VmFormComponent } from '@vm/components/vm-form/vm-form.component';
+import { VmRegisterComponent } from '@vm/components/vm-register/vm-register.component';
+
 
 export const vmRoutes: Routes = [
 	{ path: 'vms', component: VmListComponent },
-	{ path: 'vms/register', component: VmFormComponent },
-	{ path: 'vms/edit/:id', component: VmFormComponent },
-	{ path: 'vms/:id', component: VmDetailsComponent }
+	{ path: 'vms/register', component: VmRegisterComponent }
 ];
