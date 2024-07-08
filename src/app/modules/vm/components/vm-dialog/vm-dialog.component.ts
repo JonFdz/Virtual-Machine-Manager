d0a0e5566fe87fbee2fe738f190293a2ba2ff3d9
JonFdz <jonfernal@gmail.com>
2024-07-08 11:38:05 +0200

    Updated vm-dialog component to manage detail, edit, reserve and delete vm

--- src/app/modules/vm/components/vm-dialog/vm-dialog.component.ts
+++ src/app/modules/vm/components/vm-dialog/vm-dialog.component.ts
@@ -2,18 +2,20 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 import { VmDetailsComponent } from '@vm/components/vm-details/vm-details.component';
-import { VmFormComponent } from '../vm-form/vm-form.component';
 import { VmReserveComponent } from '@vm/components/vm-reserve/vm-reserve.component';
+import { VmEditComponent } from '../vm-edit/vm-edit.component';
+import { VmService } from '@vm/services/vm.service';
 
 export enum DialogType {
 	Details = 'Details',
-	Reserve = 'Reserve'
+	Reserve = 'Reserve',
+	Edit = 'Edit'
 }
 
 @Component({
 	selector: 'app-vm-dialog',
 	standalone: true,
-	imports: [VmDetailsComponent, VmFormComponent, VmReserveComponent],
+	imports: [VmDetailsComponent, VmReserveComponent, VmEditComponent],
 	templateUrl: './vm-dialog.component.html',
 	styleUrl: './vm-dialog.component.scss'
 })
@@ -21,11 +23,11 @@ export class VmDialogComponent implements OnInit {
 	contentToShow = '';
 	vmId = 0;
 
-	constructor(private dialogRef: MatDialogRef<VmDialogComponent>, @Inject(MAT_DIALOG_DATA) private data: number) { }
+	constructor(private dialogRef: MatDialogRef<VmDialogComponent>, @Inject(MAT_DIALOG_DATA) private data: number, private vmService: VmService) { }
 
 	ngOnInit(): void {
-		this.contentToShow = DialogType.Details;
 		this.vmId = this.data;
+		this.contentToShow = DialogType.Details;
 	}
 
 	openReserve(): void {
@@ -37,7 +39,9 @@ export class VmDialogComponent implements OnInit {
 	}
 
 	deleteVm(): void {
-		// Implementar la lógica para eliminar la máquina virtual
+		this.vmService.deleteVm(this.vmId).subscribe(() => {
+			this.dialogRef.close();
+		});
 	}
 
 	closeDialog(): void {
