80ad7e48c64815e738a1c5edba34ea5fe5589255
JonFdz <jonfernal@gmail.com>
2024-07-08 11:37:16 +0200

    Renamed vm-form to vm-register

---
+++ src/app/modules/vm/components/vm-register/vm-register.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { VmService } from '@vm/services/vm.service';
+import { VmStatus } from '@shared/interfaces/vm.interfaces';
+
+
+
+@Component({
+	selector: 'app-vm-register',
+	templateUrl: './vm-register.component.html',
+	styleUrls: ['./vm-register.component.scss'],
+	standalone: true,
+	imports: [ReactiveFormsModule, CommonModule]
+})
+export class VmRegisterComponent {
+	vmForm: FormGroup;
+	vmStatuses = Object.values(VmStatus);
+
+	constructor(
+		private fb: FormBuilder,
+		private vmService: VmService,
+		private router: Router
+	) {
+		this.vmForm = this.fb.group({
+			vmName: [null, Validators.required],
+			ip: [null, Validators.required],
+			dnsName: [null, Validators.required],
+			project: [null],
+			environment: [null],
+			status: [null, Validators.required],
+			comment: [null],
+			reservedUserName: [null],
+			reservedTo: [null],
+			operatingSystem: [null],
+			cpuCores: [null],
+			gpu: [null],
+			ram: [null],
+			disk: [null]
+		});
+	}
+
+	onSubmit(): void {
+		this.vmForm.value.createdAt = new Date();
+		this.vmForm.value.updatedAt = new Date();
+		this.vmService.createVm(this.vmForm.value).subscribe(() => {
+			this.router.navigate(['/']);
+		});
+	}
+}
