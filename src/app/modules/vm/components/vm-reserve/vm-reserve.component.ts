ad08bcfbb9bb6b70a76f5ac4eab5e619262937ae
JonFdz <jonfernal@gmail.com>
2024-07-10 21:34:32 +0200

    Updated logic of vm-reserve component

--- src/app/modules/vm/components/vm-reserve/vm-reserve.component.ts
+++ src/app/modules/vm/components/vm-reserve/vm-reserve.component.ts
@@ -1,38 +1,66 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, input, output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { VirtualMachine } from '@vm/models/vm.model';
 import { VmService } from '@vm/services/vm.service';
+import { VmStatus } from '@shared/interfaces/vm.interfaces';
 
 @Component({
 	selector: 'app-vm-reserve',
-	standalone: true,
-	imports: [],
 	templateUrl: './vm-reserve.component.html',
-	styleUrl: './vm-reserve.component.scss'
+	styleUrl: './vm-reserve.component.scss',
+	standalone: true,
+	imports: [ReactiveFormsModule, CommonModule]
 })
 export class VmReserveComponent implements OnInit {
 	vm: VirtualMachine = {} as VirtualMachine;
+	vmForm: FormGroup;
 	vmId = input<number>();
-	close = output<void>();
-	closeReserve = output<void>();
+	closeDialog = output<void>();
+	returnToDetails = output<void>();
 
 	constructor(
 		private vmService: VmService,
-	) { }
+		private fb: FormBuilder
+	) {
+		this.vmForm = this.fb.group({
+			reservedUserName: [null, Validators.required],
+			reservedTo: [null, Validators.required],
+			project: [null],
+			comment: [null],
+		});
+	}
 
 	ngOnInit(): void {
 		if (this.vmId()) {
 			this.vmService.getVm(this.vmId()!).subscribe(data => {
 				this.vm = data;
+				this.vmForm.patchValue({
+					reservedUserName: this.vm.reservedUserName,
+					reservedTo: this.vm.reservedTo,
+					project: this.vm.project,
+					comment: this.vm.comment
+				});
 			});
 		}
 	}
 
-	closeReserveDialog(): void {
-		this.closeReserve.emit();
+	onSubmit(): void {
+		console.log(this.vmForm.value);
+		this.vmForm.value.reservedFrom = new Date();
+		this.vmForm.value.updatedAt = new Date();
+		this.vmForm.value.status = VmStatus.Reserved;
+		this.vmService.updateVm(this.vmId()!, this.vmForm.value).subscribe(() => {
+			this.onCloseDialog();
+		});
 	}
 
-	closeDialog(): void {
-		this.close.emit();
+	onReturnToDetails(): void {
+		this.returnToDetails.emit();
+	}
+
+	onCloseDialog(): void {
+		this.closeDialog.emit();
 	}
 }
